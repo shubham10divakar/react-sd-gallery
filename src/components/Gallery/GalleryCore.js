a716af43844c644bb5cf39f0b4536a6a940c70ad
shubham10divakar <shubham.divakar@gmail.com>
2024-08-06 12:09:42 +0530

    Lazy loading disabled for more changes in development

--- src/components/Gallery/GalleryCore.js
+++ src/components/Gallery/GalleryCore.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './GalleryCore.css'
-import LazyLoad from 'react-lazyload';
+//import LazyLoad from 'react-lazyload';
 
 export const GalleryCore = ({ images, onImageClick }) => {
   return (
     <div className="gallery-container">
       {images.map((image, index) => (
         <div className="image-wrapper" key={index} onClick={() => onImageClick(index)}>
-          <LazyLoad key={index} height={200} offset={100}>
+          {/* <LazyLoad key={index} height={200} offset={100}> */}
             <img
               src={image.src}
               alt={image.alt}
               className="image-item"
               loading="lazy" // For native lazy loading
             />
-          </LazyLoad>
+          {/* </LazyLoad> */}
           <div className="image-caption">{image.caption}</div>
         </div>
       ))}
